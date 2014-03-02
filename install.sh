4924d69bc17cccf19813a1e1ad8be8498b441d45
root <dbalnaves@gmail.com>
2014-03-02 11:43:47 +0000

    bundle

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+MAC=$(ip l l eth0 | grep ether | awk '{ print $2 }')
+VPC=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$MAC/vpc-id)
+
+sed -e 's/options {/options {\n        empty-zones-enable yes;/' -i /etc/named.conf
+echo 'include "/etc/named/named.ddns.conf";' >> /etc/named.conf
+
+mkdir /etc/named/ec2-dns
+wget -P /etc/named/ec2-dns https://github.com/dbalnaves/EC2-DNS/raw/master/named.ddns.conf
+wget -P /usr/local/bin https://github.com/dbalnaves/EC2-DNS/raw/master/parse_instance.py
+chmod 755 /usr/local/bin/parse_instance.py
+
+echo "*/2 * * * * root /usr/local/bin/parse_instance.py $VPC $1 | nsupdate" > /etc/cron.d/ec2-dns
+
+/etc/init.d/named start
